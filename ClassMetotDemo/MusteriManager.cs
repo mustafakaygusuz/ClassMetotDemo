2b643cf16f4e15be2988ea666e21bbc34dc66bd2
Mustafa İlhan Kaygusuz <76887568+titanmust@users.noreply.github.com>
2021-01-20 10:20:24 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Bankamıza Kayıtlı Müşterinin Adı :  " + musteri.MusteriAd + " Soyadı : " + musteri.MusteriSoyad + " Yaşı :  " + musteri.MusteriYas);
+        }
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Aramıza Hoşgeldin : " + musteri.MusteriAd + " " + musteri.MusteriSoyad);
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Sayın " + musteri.MusteriAd + " " + musteri.MusteriSoyad + " Bankamızdaki Hesabınız silinmiştir.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.MusteriAd = "Emircan";
+            musteri1.MusteriSoyad = "Dumlu";
+            musteri1.MusteriYas = 18;
+            musteri1.MusteriTc = "12345678901";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.MusteriAd = "Ahmet Emre";
+            musteri2.MusteriSoyad = "Sakarya";
+            musteri2.MusteriYas = 17;
+            musteri1.MusteriTc = "12345678902";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.MusteriAd = "Mustafa İlhan";
+            musteri3.MusteriSoyad = "Kaygusuz";
+            musteri3.MusteriYas = 18;
+            musteri3.MusteriTc = "12345678903";
+
+            Console.WriteLine("-----------------Musteri Array-----------------");
+
+            Musteri[] musteriler = new Musteri[] {musteri1, musteri2, musteri3 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Bankamıza Kayıtlı Müşterinin Adı - Soyadı : " + musteri.MusteriAd + " " + musteri.MusteriSoyad + " - Yaşı : " + musteri.MusteriYas + " - TcNo : " +musteri.MusteriTc );
+            }
+            Console.WriteLine("-----------------Musteri Manager-----------------");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriListele(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+            musteriManager.MusteriSil(musteri1);
+
+        }
+    }
+}
